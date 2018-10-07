a815b47742a1ac52a50b429a1375c0c96dfe6909
Zannzabar <danny.sanatar@gmail.com>
2018-10-06 20:09:24 -0400

    function added for Problem 6

--- src/solutions.java
+++ src/solutions.java
@@ -1,7 +1,7 @@
 public class solutions{
 
     public static void main(String[] args){
-        System.out.print(multiples());
+        System.out.print(squareDiff());
     }
 
     public static int multiples(){
@@ -11,6 +11,17 @@ public class solutions{
                 total += i;
         }
         return total;
+    }
+
+    public static int squareDiff(){
+        int sumSq = 0;
+        int sqSum = 0;
+        for(int i = 1; i <= 100; i++){
+            sumSq += (int)Math.pow(i, 2);
+            sqSum += i;
+        }
+        sqSum = (int)Math.pow(sqSum, 2);
+        return sqSum - sumSq;
     }
 
 }
